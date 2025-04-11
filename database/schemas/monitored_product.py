50fe3ac815d402c24c3dcc09d140d056250aa0d4
Abilka <51174445+Abilka@users.noreply.github.com>
2025-04-12 02:53:59 +0300

    refactor: Изменено название поля и удалены неиспользуемые связи в схеме MonitoredProduct
    - Изменено product_id на name
    - Удалены связи с моделями User и Product
    - Обновлен метод __repr__ для отображения имени и user_id

--- database/schemas/monitored_product.py
+++ database/schemas/monitored_product.py
@@ -9,14 +9,12 @@ class MonitoredProduct(Base):
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
+    name = Column(String, nullable=False)
     url = Column(String(512), nullable=True)
-    target_price = Column(Float, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-    user = relationship("User", back_populates="monitored_products")
-    product = relationship("Product", back_populates="monitored_products")
+    user = relationship("User")
 
     def __repr__(self):
         return f"<MonitoredProduct(name='{self.name}', user_id='{self.user_id}')>" 
