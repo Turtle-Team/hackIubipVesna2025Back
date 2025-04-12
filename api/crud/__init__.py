bb289e2ca668e85f096eddbcd98c8d9d6e2f52af
Abilka <51174445+Abilka@users.noreply.github.com>
2025-04-12 11:03:06 +0300

    feat: Добавлены функции для работы с продуктами
    Добавлены импорты функций create_product, delete_product, update_product, get_product, get_products и get_product_by_market_and_item_id из модуля product.

--- api/crud/__init__.py
+++ api/crud/__init__.py
@@ -2,3 +2,4 @@ from .user import create_user, get_user, get_users, update_user, delete_user
 from .role import create_role, get_role, get_roles, update_role, delete_role
 from .auth import get_user_by_login
 from .monitored_product import get_monitored_products, create_monitored_product, delete_monitored_product, update_monitored_product, get_monitored_product
+from .product import create_product, delete_product, update_product, get_product, get_products, get_product_by_market_and_item_id
