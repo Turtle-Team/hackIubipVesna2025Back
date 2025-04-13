380003fde0ef5f2b6115b848676a6891e56e7793
Abilka <51174445+Abilka@users.noreply.github.com>
2025-04-13 10:24:44 +0300

    refactor: удаление устаревшего кода из main.py
    - Удален импорт и запуск монитора уведомлений, так как он больше не используется.
    - Удалены ненужные импорты.
    - Нет критических изменений, функциональность приложения не повреждена.

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import api
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.utils.notification import EmailNotificationSender, start_monitor
-import threading
 
 app = FastAPI()
 
@@ -18,9 +16,7 @@ app.add_middleware(
 # Include routers
 app.include_router(api.router)
 
-# Запускаем монитор уведомлений в отдельном потоке
-notification_thread = threading.Thread(target=start_monitor, daemon=True)
-notification_thread.start()
+
 
 # Тестовый модуль
 if __name__ == "__main__":
