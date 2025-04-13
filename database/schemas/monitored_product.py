a55ba666ed7dff16d9757467d7edf85debf25ffd
Abilka <51174445+Abilka@users.noreply.github.com>
2025-04-13 10:24:11 +0300

    refactor: Увеличение ограничения длины URL и добавление нового подхода к созданию мониторинга продуктов
    - Увеличена длина URL до 2048 символов.
    - Добавлен новый метод опроса продуктов через имя.
    - Обновлены механизмы создания мониторинга продуктов для поддержки нового подхода.

--- database/schemas/monitored_product.py
+++ database/schemas/monitored_product.py
@@ -10,7 +10,7 @@ class MonitoredProduct(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     name = Column(String, nullable=False)
-    url = Column(String(512), nullable=True)
+    url = Column(String(2048), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
