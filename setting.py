03a709b2e792d9028e2a1aa6b84295eb679d8905
HappyRoach <117393350+HappyRoach@users.noreply.github.com>
2025-04-13 11:45:32 +0300

    telegram notification

--- setting.py
+++ setting.py
@@ -5,3 +5,6 @@ SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 EMAIL_USER = 'happyroachs3@gmail.com'
 EMAIL_PASSWORD = 'rhgc cnzj xkkk bmea'
+
+# Telegram settings
+TELEGRAM_BOT_TOKEN = '8155103922:AAE99WHEPZy6_g9lfsFenACsG6RyQJhULpA'
